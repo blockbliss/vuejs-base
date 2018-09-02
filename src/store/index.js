87258e5e78224bf8b33458b0b61079d432e015f9
Enrique Chavez <enrique.chavez@blockbliss.com>
2018-09-01 20:44:08 -0500

    Started structure

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,8 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  strict: process.env.NODE_ENV !== 'production',
+  state: {},
+  mutations: {},
+  actions: {},
 });
